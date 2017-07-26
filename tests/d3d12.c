2ba76ceb0279651923386beb2cbfc4993af0f577
Józef Kucia <jkucia@codeweavers.com>
2017-07-26 14:50:39 +0200

    tests: Do not assign specific shader registers in test_texture().
    It is not needed since we have a basic mapping for D3D12 shader
    registers.

--- tests/d3d12.c
+++ tests/d3d12.c
@@ -829,7 +829,7 @@ static ID3D12RootSignature *create_texture_root_signature_(unsigned int line,
     sampler_desc.AddressU = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
     sampler_desc.AddressV = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
     sampler_desc.AddressW = D3D12_TEXTURE_ADDRESS_MODE_CLAMP;
-    sampler_desc.ShaderRegister = 1;
+    sampler_desc.ShaderRegister = 0;
     sampler_desc.RegisterSpace = 0;
     sampler_desc.ShaderVisibility = shader_visibility;
 
@@ -6653,8 +6653,8 @@ static void test_texture(void)
     static const DWORD ps_code[] =
     {
 #if 0
-        Texture2D t : register(t0);
-        SamplerState s : register(s1);
+        Texture2D t;
+        SamplerState s;
 
         float4 main(float4 position : SV_POSITION) : SV_Target
         {
@@ -6665,16 +6665,16 @@ static void test_texture(void)
             return t.Sample(s, p);
         }
 #endif
-        0x43425844, 0xc5df85e3, 0x32854510, 0xedd54782, 0x20bf84ff, 0x00000001, 0x00000140, 0x00000003,
+        0x43425844, 0x7a0c3929, 0x75ff3ca4, 0xccb318b2, 0xe6965b4c, 0x00000001, 0x00000140, 0x00000003,
         0x0000002c, 0x00000060, 0x00000094, 0x4e475349, 0x0000002c, 0x00000001, 0x00000008, 0x00000020,
         0x00000000, 0x00000001, 0x00000003, 0x00000000, 0x0000030f, 0x505f5653, 0x5449534f, 0x004e4f49,
         0x4e47534f, 0x0000002c, 0x00000001, 0x00000008, 0x00000020, 0x00000000, 0x00000000, 0x00000003,
         0x00000000, 0x0000000f, 0x545f5653, 0x65677261, 0xabab0074, 0x58454853, 0x000000a4, 0x00000050,
-        0x00000029, 0x0100086a, 0x0300005a, 0x00106000, 0x00000001, 0x04001858, 0x00107000, 0x00000000,
+        0x00000029, 0x0100086a, 0x0300005a, 0x00106000, 0x00000000, 0x04001858, 0x00107000, 0x00000000,
         0x00005555, 0x04002064, 0x00101032, 0x00000000, 0x00000001, 0x03000065, 0x001020f2, 0x00000000,
         0x02000068, 0x00000001, 0x0a000038, 0x00100032, 0x00000000, 0x00101046, 0x00000000, 0x00004002,
         0x3d000000, 0x3d000000, 0x00000000, 0x00000000, 0x8b000045, 0x800000c2, 0x00155543, 0x001020f2,
-        0x00000000, 0x00100046, 0x00000000, 0x00107e46, 0x00000000, 0x00106000, 0x00000001, 0x0100003e,
+        0x00000000, 0x00100046, 0x00000000, 0x00107e46, 0x00000000, 0x00106000, 0x00000000, 0x0100003e,
     };
     static const D3D12_SHADER_BYTECODE ps = {ps_code, sizeof(ps_code)};
     static const float red[] = {1.0f, 0.0f, 0.0f, 0.5f};
