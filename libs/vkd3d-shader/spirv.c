6ffe54b9ec2e0b5a07a9f191b13559ca7ddf8148
Józef Kucia <jkucia@codeweavers.com>
2017-08-22 12:39:56 +0200

    libs/vkd3d-shader: Translate round_z instructions.

--- libs/vkd3d-shader/spirv.c
+++ libs/vkd3d-shader/spirv.c
@@ -3287,6 +3287,7 @@ static enum GLSLstd450 vkd3d_dxbc_compiler_map_ext_glsl_instruction(
         {VKD3DSIH_MIN,             GLSLstd450FMin},
         {VKD3DSIH_ROUND_NI,        GLSLstd450Floor},
         {VKD3DSIH_ROUND_PI,        GLSLstd450Ceil},
+        {VKD3DSIH_ROUND_Z,         GLSLstd450Trunc},
         {VKD3DSIH_RSQ,             GLSLstd450InverseSqrt},
         {VKD3DSIH_SQRT,            GLSLstd450Sqrt},
         {VKD3DSIH_UMAX,            GLSLstd450UMax},
@@ -4613,6 +4614,7 @@ void vkd3d_dxbc_compiler_handle_instruction(struct vkd3d_dxbc_compiler *compiler
         case VKD3DSIH_MIN:
         case VKD3DSIH_ROUND_NI:
         case VKD3DSIH_ROUND_PI:
+        case VKD3DSIH_ROUND_Z:
         case VKD3DSIH_RSQ:
         case VKD3DSIH_SQRT:
         case VKD3DSIH_UMAX:
