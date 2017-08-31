f3862db04d43540d43c7239b3777ebe40177808f
Józef Kucia <jkucia@codeweavers.com>
2017-08-31 09:29:02 +0200

    libs/vkd3d: Use VK_PIPELINE_BIND_POINT_RANGE_SIZE.

--- libs/vkd3d/vkd3d_private.h
+++ libs/vkd3d/vkd3d_private.h
@@ -463,7 +463,7 @@ struct d3d12_command_list
 
     VkFramebuffer current_framebuffer;
     VkPipeline current_pipeline;
-    struct vkd3d_pipeline_bindings pipeline_bindings[VK_PIPELINE_BIND_POINT_COMPUTE + 1];
+    struct vkd3d_pipeline_bindings pipeline_bindings[VK_PIPELINE_BIND_POINT_RANGE_SIZE];
 
     struct d3d12_pipeline_state *state;
 
