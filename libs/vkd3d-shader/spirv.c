602f9780f81eca94624f3b7210984b05410c183a
Józef Kucia <jkucia@codeweavers.com>
2017-09-14 15:38:19 +0200

    libs/vkd3d-shader: Implement "sample" interpolation qualifier.

--- libs/vkd3d-shader/spirv.c
+++ libs/vkd3d-shader/spirv.c
@@ -3386,6 +3386,10 @@ static void vkd3d_dxbc_compiler_emit_interpolation_decorations(struct vkd3d_dxbc
             break;
         case VKD3DSIM_LINEAR:
             break;
+        case VKD3DSIM_LINEAR_SAMPLE:
+            vkd3d_spirv_enable_capability(builder, SpvCapabilitySampleRateShading);
+            vkd3d_spirv_build_op_decorate(builder, id, SpvDecorationSample, NULL, 0);
+            break;
         default:
             FIXME("Unhandled interpolation mode %#x.\n", mode);
             break;
