225242c0b1120642a30c5da2cee64656d9a1535f
Zhiyi Zhang <zzhang@codeweavers.com>
2019-01-31 17:07:28 +0800

    vkd3d: Implement d3d12_pipeline_state_SetName().
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- libs/vkd3d/state.c
+++ libs/vkd3d/state.c
@@ -1195,9 +1195,9 @@ static HRESULT STDMETHODCALLTYPE d3d12_pipeline_state_SetName(ID3D12PipelineStat
 {
     struct d3d12_pipeline_state *state = impl_from_ID3D12PipelineState(iface);
 
-    FIXME("iface %p, name %s stub!\n", iface, debugstr_w(name, state->device->wchar_size));
+    TRACE("iface %p, name %s.\n", iface, debugstr_w(name, state->device->wchar_size));
 
-    return E_NOTIMPL;
+    return name ? S_OK : E_INVALIDARG;
 }
 
 static HRESULT STDMETHODCALLTYPE d3d12_pipeline_state_GetDevice(ID3D12PipelineState *iface,
