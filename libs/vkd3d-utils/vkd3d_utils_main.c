40f920c3ccf0f52a5616867895022f67e9ae5ec0
Hans-Kristian Arntzen <post@arntzen-software.no>
2019-10-15 16:01:06 +0200

    vkd3d-utils: Make vkd3d-utils compilable on MSVC.
    Useful to be able to run unit tests on Windows.
    
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d-utils/vkd3d_utils_main.c
+++ libs/vkd3d-utils/vkd3d_utils_main.c
@@ -16,6 +16,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#include "vkd3d_common.h"
 #include "vkd3d_utils_private.h"
 
 VKD3D_DEBUG_ENV_NAME("VKD3D_DEBUG");
