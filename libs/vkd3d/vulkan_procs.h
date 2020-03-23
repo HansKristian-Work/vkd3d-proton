577efe90406fbdaf31be118b8f4bc06e0d0bc9fe
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2020-03-23 16:38:52 +0100

    vkd3d: Optionally enable VK_KHR_buffer_device_address.
    Signed-off-by: Philip Rebohle <philip.rebohle@tu-dortmund.de>

--- libs/vkd3d/vulkan_procs.h
+++ libs/vkd3d/vulkan_procs.h
@@ -177,6 +177,11 @@ VK_DEVICE_PFN(vkUnmapMemory)
 VK_DEVICE_PFN(vkUpdateDescriptorSets)
 VK_DEVICE_PFN(vkWaitForFences)
 
+/* VK_KHR_buffer_device_address */
+VK_DEVICE_EXT_PFN(vkGetBufferDeviceAddressKHR);
+VK_DEVICE_EXT_PFN(vkGetBufferOpaqueCaptureAddressKHR);
+VK_DEVICE_EXT_PFN(vkGetDeviceMemoryOpaqueCaptureAddressKHR);
+
 /* VK_KHR_draw_indirect_count */
 VK_DEVICE_EXT_PFN(vkCmdDrawIndirectCountKHR)
 VK_DEVICE_EXT_PFN(vkCmdDrawIndexedIndirectCountKHR)
