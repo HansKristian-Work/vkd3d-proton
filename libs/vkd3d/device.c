0a6415d6773bb50d96816c8d652181f061bf8c88
Conor McCarthy <conor.mccarthy.444@gmail.com>
2020-05-25 20:07:09 +0430

    vkd3d: Implement support for D3D12_FEATURE_SERIALIZATION.
    Signed-off-by: Conor McCarthy <conor.mccarthy.444@gmail.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- libs/vkd3d/device.c
+++ libs/vkd3d/device.c
@@ -2909,6 +2909,28 @@ static HRESULT STDMETHODCALLTYPE d3d12_device_CheckFeatureSupport(ID3D12Device *
             return S_OK;
         }
 
+        case D3D12_FEATURE_SERIALIZATION:
+        {
+            D3D12_FEATURE_DATA_SERIALIZATION *data = feature_data;
+
+            if (feature_data_size != sizeof(*data))
+            {
+                WARN("Invalid size %u.\n", feature_data_size);
+                return E_INVALIDARG;
+            }
+
+            if (data->NodeIndex)
+            {
+                FIXME("Multi-adapter not supported.\n");
+                return E_INVALIDARG;
+            }
+
+            data->HeapSerializationTier = D3D12_HEAP_SERIALIZATION_TIER_0;
+
+            TRACE("Heap serialisation tier %#x.\n", data->HeapSerializationTier);
+            return S_OK;
+        }
+
         default:
             FIXME("Unhandled feature %#x.\n", feature);
             return E_NOTIMPL;
