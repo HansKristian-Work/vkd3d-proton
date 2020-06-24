a0d232c10cda4da017b24a86abcdc34c322057fe
Joshua Ashton <joshua@froggi.es>
2020-06-24 10:46:23 +0100

    vkd3d: Implement sequential consistency on MSVC
    Also optimize and reduce unnecessary barriers.
    
    Signed-off-by: Joshua Ashton <joshua@froggi.es>

--- include/private/vkd3d_atomic.h
+++ include/private/vkd3d_atomic.h
@@ -28,34 +28,70 @@
 typedef enum
 {
     memory_order_relaxed,
+    memory_order_consume,
     memory_order_acquire,
     memory_order_release,
     memory_order_acq_rel,
+    memory_order_seq_cst,
 } memory_order;
 
 # define vkd3d_atomic_rw_barrier() _ReadWriteBarrier()
-# define vkd3d_atomic_rw_barrier_acquire(order) { if (order == memory_order_acquire || order == memory_order_acq_rel) { vkd3d_atomic_rw_barrier(); } }
-# define vkd3d_atomic_rw_barrier_release(order) { if (order == memory_order_release || order == memory_order_acq_rel) { vkd3d_atomic_rw_barrier(); } }
+
+FORCEINLINE void vkd3d_atomic_load_barrier(memory_order order)
+{
+    switch (order)
+    {
+        case memory_order_consume:
+        case memory_order_acquire:
+        case memory_order_seq_cst:
+            vkd3d_atomic_rw_barrier();
+            break;
+
+        case memory_order_relaxed:
+        default:
+            break;
+    }
+}
+
+// Redefinitions for invalid memory orders...
+#define InterlockedExchangeRelease InterlockedExchange
+
+#define vkd3d_atomic_choose_intrinsic(order, result, intrinsic, ...)                        \
+    switch (order)                                                                          \
+    {                                                                                       \
+        case memory_order_relaxed: result = intrinsic##NoFence  (__VA_ARGS__); break;       \
+        case memory_order_consume:                                                          \
+        case memory_order_acquire: result = intrinsic##Acquire (__VA_ARGS__); break;        \
+        case memory_order_release: result = intrinsic##Release (__VA_ARGS__); break;        \
+        case memory_order_acq_rel:                                                          \
+        case memory_order_seq_cst: result = intrinsic          (__VA_ARGS__); break;        \
+    }
 
 FORCEINLINE uint32_t vkd3d_uint32_atomic_load_explicit(uint32_t *target, memory_order order)
 {
     uint32_t value = *((volatile uint32_t*)target);
-    vkd3d_atomic_rw_barrier_acquire(order);
+    vkd3d_atomic_load_barrier(order);
     return value;
 }
 
 FORCEINLINE void vkd3d_uint32_atomic_store_explicit(uint32_t *target, uint32_t value, memory_order order)
 {
-    vkd3d_atomic_rw_barrier_release(order);
-    *((volatile uint32_t*)target) = value;
+    switch (order)
+    {
+        case memory_order_release: vkd3d_atomic_rw_barrier(); // fallthrough...
+        case memory_order_relaxed: *((volatile uint32_t*)target) = value; break;
+        default:
+        case memory_order_seq_cst:
+            (void) InterlockedExchange((LONG*) target, value);
+    }
+    
 }
 
 FORCEINLINE uint32_t vkd3d_uint32_atomic_exchange_explicit(uint32_t *target, uint32_t value, memory_order order)
 {
-    vkd3d_atomic_rw_barrier_release(order);
-    uint32_t oldValue = InterlockedExchange((LONG*)target, value);
-    vkd3d_atomic_rw_barrier_acquire(order);
-    return oldValue;
+    uint32_t result;
+    vkd3d_atomic_choose_intrinsic(order, result, InterlockedExchange, (LONG*)target, value);
+    return result;
 }
 
 #elif defined(__GNUC__) || defined(__clang__)
