8cc0abfca0ed7457f63f3f977c3049756177ef5b
Henri Verbeet <hverbeet@codeweavers.com>
2020-09-21 19:48:28 +0330

    vkd3d-shader: Rename the "SHOW_BYTE_OFFSET" output formatting flag to just "OFFSETS".
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- programs/vkd3d-compiler/main.c
+++ programs/vkd3d-compiler/main.c
@@ -149,7 +149,7 @@ static void print_usage(const char *program_name)
         "                        standard output.\n"
         "  --formatting=<type>   Specify the formatting options for text output.\n"
         "                        Valid values are 'none', 'print', 'colour', 'indent',\n"
-        "                        'show-byte-offset', 'no-header', and 'friendly-names'.\n"
+        "                        'offsets', 'no-header', and 'friendly-names'.\n"
         "                        Default is --formatting=friendly-names,indent.\n"
         "  --print-source-types  Display the supported source types and exit.\n"
         "  --print-target-types  Display the supported target types for the specified\n"
@@ -238,7 +238,7 @@ static bool parse_formatting(uint32_t *formatting, char *arg)
         {"none",             VKD3D_SHADER_COMPILE_OPTION_FORMATTING_NONE            },
         {"colour",           VKD3D_SHADER_COMPILE_OPTION_FORMATTING_COLOUR          },
         {"indent",           VKD3D_SHADER_COMPILE_OPTION_FORMATTING_INDENT          },
-        {"show-byte-offset", VKD3D_SHADER_COMPILE_OPTION_FORMATTING_SHOW_BYTE_OFFSET},
+        {"offsets",          VKD3D_SHADER_COMPILE_OPTION_FORMATTING_OFFSETS         },
         {"no-header",        VKD3D_SHADER_COMPILE_OPTION_FORMATTING_NO_HEADER       },
         {"friendly-names",   VKD3D_SHADER_COMPILE_OPTION_FORMATTING_FRIENDLY_NAMES  },
     };
