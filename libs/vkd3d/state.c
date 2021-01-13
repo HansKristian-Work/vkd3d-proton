4b05e77a2b8175582378c0d6f37dbde4447c7ce6
Hans-Kristian Arntzen <post@arntzen-software.no>
2021-01-13 11:54:18 +0100

    vkd3d: Allow a heap binding to cover multiple descriptors.
    This begins the refactor toward letting us to use both texel buffer and
    SSBO descriptors for typed buffers, which is a better workaround than
    force_bindless_texel_buffers.
    
    In this new approach, we store a mask in metadata instead of
    set/binding.
    
    When copying a descriptor, we will iterate over the masks and look up
    binding directly from device->bindless_state.set_info[].
    
    The mask is represented in terms of info index rather than set index to
    avoid needless lookups. Add some new helpers to make this process
    easier.
    
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/state.c
+++ libs/vkd3d/state.c
@@ -3530,6 +3530,11 @@ static HRESULT vkd3d_bindless_state_add_binding(struct vkd3d_bindless_state *bin
     set_info->flags = flags;
     set_info->binding_index = vkd3d_popcount(flags & VKD3D_BINDLESS_SET_EXTRA_MASK);
 
+    if (set_info->heap_type == D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV)
+        set_info->set_index = bindless_state->cbv_srv_uav_count++;
+    else
+        set_info->set_index = 0;
+
     for (i = 0; i < set_info->binding_index; i++)
     {
         /* all extra bindings are storage buffers right now */
@@ -3901,4 +3906,33 @@ struct vkd3d_descriptor_binding vkd3d_bindless_state_find_set(const struct vkd3d
     binding.set = 0;
     binding.binding = 0;
     return binding;
+}
+
+struct vkd3d_descriptor_binding vkd3d_bindless_state_binding_from_info_index(
+        const struct vkd3d_bindless_state *bindless_state, uint32_t index)
+{
+    struct vkd3d_descriptor_binding binding;
+    binding.binding = bindless_state->set_info[index].binding_index;
+    binding.set = bindless_state->set_info[index].set_index;
+    return binding;
+}
+
+uint32_t vkd3d_bindless_state_find_set_info_index(const struct vkd3d_bindless_state *bindless_state, uint32_t flags)
+{
+    D3D12_DESCRIPTOR_HEAP_TYPE heap_type;
+    unsigned int i;
+
+    heap_type = flags & VKD3D_BINDLESS_SET_SAMPLER
+                ? D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER
+                : D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+
+    for (i = 0; i < bindless_state->set_count; i++)
+    {
+        const struct vkd3d_bindless_set_info *set_info = &bindless_state->set_info[i];
+        if (set_info->heap_type == heap_type && (set_info->flags & flags) == flags)
+            return i;
+    }
+
+    ERR("No set found for flags %#x.", flags);
+    return 0;
 }
