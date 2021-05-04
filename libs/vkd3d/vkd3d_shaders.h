4f0872152ac6cbf0cc702a9cb0f7e33fec9cf974
Hans-Kristian Arntzen <post@arntzen-software.no>
2021-05-04 14:40:14 +0200

    meta: Add fs_copy_uint path.
    For stencil -> color copies.
    
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/vkd3d_shaders.h
+++ libs/vkd3d/vkd3d_shaders.h
@@ -49,6 +49,7 @@ enum vkd3d_meta_copy_mode
 #include <vs_fullscreen.h>
 #include <gs_fullscreen.h>
 #include <fs_copy_image_float.h>
+#include <fs_copy_image_uint.h>
 #include <vs_swapchain_fullscreen.h>
 #include <fs_swapchain_fullscreen.h>
 
