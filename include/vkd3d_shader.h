ced71f841ec114d352163b4b3185740d9d71ab5b
Joshua Ashton <joshua@froggi.es>
2021-07-08 15:26:24 +0100

    vkd3d-shader: Restrict descriptor-qa extras and logic to VKD3D_ENABLE_DESCRIPTOR_QA
    Signed-off-by: Joshua Ashton <joshua@froggi.es>

--- include/vkd3d_shader.h
+++ include/vkd3d_shader.h
@@ -203,10 +203,12 @@ struct vkd3d_shader_interface_info
     /* Ignored unless VKD3D_SHADER_INTERFACE_SSBO_OFFSET_BUFFER or TYPED_OFFSET_BUFFER is set */
     const struct vkd3d_shader_descriptor_binding *offset_buffer_binding;
 
+#ifdef VKD3D_ENABLE_DESCRIPTOR_QA
     /* Ignored unless VKD3D_SHADER_INTERFACE_DESCRIPTOR_QA_BUFFER is set. */
     const struct vkd3d_shader_descriptor_binding *descriptor_qa_global_binding;
     /* Ignored unless VKD3D_SHADER_INTERFACE_DESCRIPTOR_QA_BUFFER is set. */
     const struct vkd3d_shader_descriptor_binding *descriptor_qa_heap_binding;
+#endif
 
     VkShaderStageFlagBits stage;
 
