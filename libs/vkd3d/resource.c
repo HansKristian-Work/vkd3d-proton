HansKristian-Work/vkd3d-proton

commit 9983a1720f683b6366a9cf7a864a6efc94978e1d

vkd3d: Splat null descriptors to all sets.
Some games end up writing the wrong descriptor type when using null
descriptors, and to be robust against that, we have to clear out
all descriptors when creating null descriptors.

If we copy a null descriptor, we will also have to copy from all sets.

Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

libs/vkd3d/vkd3d_private.h
@@ -1054,7 +1054,25 @@ struct vkd3d_host_visible_buffer_range
     void *host_ptr;
 };
 
+union vkd3d_descriptor_info
+{
+    VkBufferView buffer_view;
+    VkDescriptorBufferInfo buffer;
+    VkDescriptorImageInfo image;
+    VkDeviceAddress va;
+};
+
 /* ID3D12DescriptorHeap */
+struct d3d12_null_descriptor_template
+{
+    struct VkWriteDescriptorSet writes[VKD3D_MAX_BINDLESS_DESCRIPTOR_SETS];
+    VkDescriptorBufferInfo buffer;
+    VkDescriptorImageInfo image;
+    VkBufferView buffer_view;
+    unsigned int num_writes;
+    unsigned int set_info_mask;
+};
+
 struct d3d12_descriptor_heap
 {
     ID3D12DescriptorHeap ID3D12DescriptorHeap_iface;
@@ -1076,6 +1094,8 @@ struct d3d12_descriptor_heap
     struct vkd3d_host_visible_buffer_range descriptor_heap_info;
     uint64_t cookie;
 #endif
+
+    struct d3d12_null_descriptor_template null_descriptor_template;
 
     struct d3d12_device *device;
 
@@ -1556,14 +1576,6 @@ enum vkd3d_pipeline_dirty_flag
     VKD3D_PIPELINE_DIRTY_STATIC_SAMPLER_SET       = 0x00000001u,
     VKD3D_PIPELINE_DIRTY_DESCRIPTOR_TABLE_OFFSETS = 0x00000002u,
     VKD3D_PIPELINE_DIRTY_HOISTED_DESCRIPTORS      = 0x00000004u,
-};
-
-union vkd3d_descriptor_info
-{
-    VkBufferView buffer_view;
-    VkDescriptorBufferInfo buffer;
-    VkDescriptorImageInfo image;
-    VkDeviceAddress va;
 };
 
 struct vkd3d_root_descriptor_info

commit 10e1e4d89ca7cc6a7b00ee33830921eace5fa3aa

vkd3d: Optimize repeated null descriptor updates
There are titles clearing the same descriptors constantly.
This leads to unnecessary updates that can become costly.

This commit introduces a new flag to track when D3D12 descriptors are
not null, and skips clearing them if they are already null.
Descriptors are assumed to be null by default.

This fixes a performance regression introduced by
9983a1720f683b6366a9cf7a864a6efc94978e1d

Signed-off-by: Rodrigo Locatti <rlocatti@nvidia.com>

libs/vkd3d/resource.c
@@ -3494,6 +3494,18 @@ static void d3d12_descriptor_heap_write_null_descriptor_template(struct d3d12_de
     VkDeviceAddress *va;
 
     heap = desc->heap;
+
+    /* When mutable descriptors are not supported, set a dummy type.
+       This will make those drivers not care about the null type being different between
+       null writes. */
+    if (!heap->null_descriptor_template.has_mutable_descriptors)
+        vk_mutable_descriptor_type = 0;
+
+    /* Skip writes with the same null type that are already null. */
+    if (!(desc->metadata.flags & VKD3D_DESCRIPTOR_FLAG_NON_NULL)
+            && desc->current_null_type == vk_mutable_descriptor_type)
+        return;
+
     num_writes = heap->null_descriptor_template.num_writes;
     vk_procs = &heap->device->vk_procs;
     offset = desc->heap_offset;
@@ -3511,6 +3523,7 @@ static void d3d12_descriptor_heap_write_null_descriptor_template(struct d3d12_de
     desc->metadata.cookie = 0;
     desc->metadata.flags = 0;
     desc->metadata.set_info_mask = heap->null_descriptor_template.set_info_mask;
+    desc->current_null_type = vk_mutable_descriptor_type;
     memset(&desc->info, 0, sizeof(desc->info));
 
     va = heap->raw_va_aux_buffer.host_ptr;
@@ -3569,7 +3582,7 @@ void d3d12_desc_create_cbv(struct d3d12_desc *descriptor,
 
     descriptor->metadata.cookie = resource ? resource->cookie : 0;
     descriptor->metadata.set_info_mask = 1u << info_index;
-    descriptor->metadata.flags = VKD3D_DESCRIPTOR_FLAG_OFFSET_RANGE;
+    descriptor->metadata.flags = VKD3D_DESCRIPTOR_FLAG_OFFSET_RANGE | VKD3D_DESCRIPTOR_FLAG_NON_NULL;
     descriptor->info.buffer = descriptor_info.buffer;
 
     vkd3d_init_write_descriptor_set(&vk_write, descriptor,
@@ -3749,7 +3762,8 @@ static void vkd3d_create_buffer_srv(struct d3d12_desc *descriptor,
             VkDeviceAddress *raw_addresses = descriptor->heap->raw_va_aux_buffer.host_ptr;
             uint32_t descriptor_index = d3d12_desc_heap_offset(descriptor);
             raw_addresses[descriptor_index] = desc->RaytracingAccelerationStructure.Location;
-            descriptor->metadata.flags = VKD3D_DESCRIPTOR_FLAG_RAW_VA_AUX_BUFFER;
+            descriptor->metadata.flags = VKD3D_DESCRIPTOR_FLAG_RAW_VA_AUX_BUFFER |
+                                         VKD3D_DESCRIPTOR_FLAG_NON_NULL;
             descriptor->metadata.set_info_mask = 0;
             /* There is no resource tied to this descriptor, just a naked pointer. */
             descriptor->metadata.cookie = 0;
@@ -3798,7 +3812,8 @@ static void vkd3d_create_buffer_srv(struct d3d12_desc *descriptor,
         descriptor->metadata.cookie = resource ? resource->res.cookie : 0;
         descriptor->metadata.set_info_mask |= 1u << info_index;
 
-        descriptor->metadata.flags |= VKD3D_DESCRIPTOR_FLAG_OFFSET_RANGE;
+        descriptor->metadata.flags |= VKD3D_DESCRIPTOR_FLAG_OFFSET_RANGE |
+                                      VKD3D_DESCRIPTOR_FLAG_NON_NULL;
         if (device->bindless_state.flags & VKD3D_SSBO_OFFSET_BUFFER)
             descriptor->metadata.flags |= VKD3D_DESCRIPTOR_FLAG_BUFFER_OFFSET;
 
@@ -3828,7 +3843,7 @@ static void vkd3d_create_buffer_srv(struct d3d12_desc *descriptor,
     descriptor->metadata.cookie = view ? view->cookie : 0;
     descriptor->metadata.set_info_mask |= 1u << info_index;
 
-    descriptor->metadata.flags |= VKD3D_DESCRIPTOR_FLAG_VIEW;
+    descriptor->metadata.flags |= VKD3D_DESCRIPTOR_FLAG_VIEW | VKD3D_DESCRIPTOR_FLAG_NON_NULL;
     if (device->bindless_state.flags & VKD3D_TYPED_OFFSET_BUFFER)
         descriptor->metadata.flags |= VKD3D_DESCRIPTOR_FLAG_BUFFER_OFFSET;
 
@@ -3974,7 +3989,7 @@ static void vkd3d_create_texture_srv(struct d3d12_desc *descriptor,
     descriptor->info.view = view;
     descriptor->metadata.cookie = view ? view->cookie : 0;
     descriptor->metadata.set_info_mask = 1u << info_index;
-    descriptor->metadata.flags = VKD3D_DESCRIPTOR_FLAG_VIEW;
+    descriptor->metadata.flags = VKD3D_DESCRIPTOR_FLAG_VIEW | VKD3D_DESCRIPTOR_FLAG_NON_NULL;
 
     vkd3d_init_write_descriptor_set(&vk_write, descriptor,
             vkd3d_bindless_state_binding_from_info_index(&device->bindless_state, info_index),
@@ -4087,7 +4102,8 @@ static void vkd3d_create_buffer_uav(struct d3d12_desc *descriptor, struct d3d12_
     flags = vkd3d_view_flags_from_d3d12_buffer_uav_flags(desc->Buffer.Flags);
 
     descriptor->metadata.set_info_mask = 0;
-    descriptor->metadata.flags = VKD3D_DESCRIPTOR_FLAG_RAW_VA_AUX_BUFFER;
+    descriptor->metadata.flags = VKD3D_DESCRIPTOR_FLAG_RAW_VA_AUX_BUFFER |
+                                 VKD3D_DESCRIPTOR_FLAG_NON_NULL;
 
     if (d3d12_device_use_ssbo_raw_buffer(device))
     {
@@ -4298,7 +4314,7 @@ static void vkd3d_create_texture_uav(struct d3d12_desc *descriptor,
     descriptor->info.view = view;
     descriptor->metadata.cookie = view ? view->cookie : 0;
     descriptor->metadata.set_info_mask = 1u << info_index;
-    descriptor->metadata.flags = VKD3D_DESCRIPTOR_FLAG_VIEW;
+    descriptor->metadata.flags = VKD3D_DESCRIPTOR_FLAG_VIEW | VKD3D_DESCRIPTOR_FLAG_NON_NULL;
 
     vkd3d_init_write_descriptor_set(&vk_write, descriptor,
             vkd3d_bindless_state_binding_from_info_index(&device->bindless_state, info_index),
@@ -4603,7 +4619,7 @@ void d3d12_desc_create_sampler(struct d3d12_desc *sampler,
     sampler->info.view = view;
     sampler->metadata.cookie = view->cookie;
     sampler->metadata.set_info_mask = 1u << info_index;
-    sampler->metadata.flags = VKD3D_DESCRIPTOR_FLAG_VIEW;
+    sampler->metadata.flags = VKD3D_DESCRIPTOR_FLAG_VIEW | VKD3D_DESCRIPTOR_FLAG_NON_NULL;
 
     descriptor_info.image.sampler = view->vk_sampler;
     descriptor_info.image.imageView = VK_NULL_HANDLE;
@@ -5370,6 +5386,8 @@ static void d3d12_descriptor_heap_add_null_descriptor_template(
         descriptor_heap->null_descriptor_template.image.imageLayout = VK_IMAGE_LAYOUT_UNDEFINED;
         descriptor_heap->null_descriptor_template.image.imageView = VK_NULL_HANDLE;
         descriptor_heap->null_descriptor_template.buffer_view = VK_NULL_HANDLE;
+        descriptor_heap->null_descriptor_template.has_mutable_descriptors =
+                descriptor_heap->device->vk_info.VALVE_mutable_descriptor_type;
     }
 
     descriptor_heap->null_descriptor_template.num_writes++;

libs/vkd3d/vkd3d_private.h
@@ -961,6 +961,7 @@ enum vkd3d_descriptor_flag
     VKD3D_DESCRIPTOR_FLAG_RAW_VA_AUX_BUFFER = (1 << 1),
     VKD3D_DESCRIPTOR_FLAG_BUFFER_OFFSET     = (1 << 2),
     VKD3D_DESCRIPTOR_FLAG_OFFSET_RANGE      = (1 << 3),
+    VKD3D_DESCRIPTOR_FLAG_NON_NULL          = (1 << 4),
 };
 
 struct vkd3d_descriptor_binding
@@ -988,6 +989,7 @@ struct d3d12_desc
     DECLSPEC_ALIGN(D3D12_DESC_ALIGNMENT) struct vkd3d_descriptor_data metadata;
     struct d3d12_descriptor_heap *heap;
     uint32_t heap_offset;
+    VkDescriptorType current_null_type;
     union
     {
         VkDescriptorBufferInfo buffer;
@@ -1080,6 +1082,7 @@ struct d3d12_null_descriptor_template
     VkBufferView buffer_view;
     unsigned int num_writes;
     unsigned int set_info_mask;
+    bool has_mutable_descriptors;
 };
 
 struct d3d12_descriptor_heap