21bf0779c7a28ebdf245d34b127752ba39c3ccac
Hans-Kristian Arntzen <post@arntzen-software.no>
2021-09-16 16:28:09 +0200

    vkd3d: Fix compile error introduced by bad rebase.
    Somehow the rebase got really screwed up :\
    
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/vkd3d_private.h
+++ libs/vkd3d/vkd3d_private.h
@@ -2226,11 +2226,6 @@ struct vkd3d_memory_info_domain
     uint32_t buffer_type_mask;
     uint32_t sampled_type_mask;
     uint32_t rt_ds_type_mask;
-
-    uint32_t budget_sensitive_mask;
-    VkDeviceSize type_budget[VK_MAX_MEMORY_TYPES];
-    VkDeviceSize type_current[VK_MAX_MEMORY_TYPES];
-    pthread_mutex_t budget_lock;
 };
 
 struct vkd3d_memory_info
@@ -2246,6 +2241,11 @@ struct vkd3d_memory_info
      * Used when we want to allocate DEFAULT heaps or non-visible CUSTOM heaps.
      * For images, we only include memory types which are OPTIMAL tiled. */
     struct vkd3d_memory_info_domain non_cpu_accessible_domain;
+
+    uint32_t budget_sensitive_mask;
+    VkDeviceSize type_budget[VK_MAX_MEMORY_TYPES];
+    VkDeviceSize type_current[VK_MAX_MEMORY_TYPES];
+    pthread_mutex_t budget_lock;
 };
 
 HRESULT vkd3d_memory_info_init(struct vkd3d_memory_info *info,
