520e92d6505d947bef1f140ee93b4086db959290
Hans-Kristian Arntzen <post@arntzen-software.no>
2021-10-26 19:14:12 +0200

    tests: Test semantics for quad ops in SM 6.6.
    Depending on the shader model used, quads are assigned to lanes differently.
    
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- tests/d3d12_tests.h
+++ tests/d3d12_tests.h
@@ -273,6 +273,7 @@ decl_test(test_shader_fp16);
 decl_test(test_shader_sm62_denorm);
 decl_test(test_shader_sm64_packed);
 decl_test(test_shader_sm65_wave_intrinsics);
+decl_test(test_shader_sm66_quad_op_semantics);
 decl_test(test_get_copyable_footprints_planar);
 decl_test(test_depth_stencil_test_no_dsv);
 decl_test(test_copy_buffer_to_depth_stencil);
