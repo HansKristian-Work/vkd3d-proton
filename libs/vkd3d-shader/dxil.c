e605d19ef7989ddd927ca75a92e3106fc652f5b1
Hans-Kristian Arntzen <post@arntzen-software.no>
2021-11-09 12:15:02 +0100

    vkd3d-shader: Add shader extension for scalar block layout.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d-shader/dxil.c
+++ libs/vkd3d-shader/dxil.c
@@ -742,6 +742,17 @@ int vkd3d_shader_compile_dxil(const struct vkd3d_shader_code *dxbc,
                     goto end;
                 }
             }
+            else if (compiler_args->target_extensions[i] == VKD3D_SHADER_TARGET_EXTENSION_SCALAR_BLOCK_LAYOUT)
+            {
+                static const dxil_spv_option_scalar_block_layout helper =
+                        { { DXIL_SPV_OPTION_SCALAR_BLOCK_LAYOUT }, DXIL_SPV_TRUE };
+                if (dxil_spv_converter_add_option(converter, &helper.base) != DXIL_SPV_SUCCESS)
+                {
+                    ERR("dxil-spirv does not support SCALAR_BLOCK_LAYOUT.\n");
+                    ret = VKD3D_ERROR_NOT_IMPLEMENTED;
+                    goto end;
+                }
+            }
         }
 
         if (compiler_args->dual_source_blending)
@@ -1188,6 +1199,17 @@ int vkd3d_shader_compile_dxil_export(const struct vkd3d_shader_code *dxil,
                 if (dxil_spv_converter_add_option(converter, &helper.base) != DXIL_SPV_SUCCESS)
                 {
                     ERR("dxil-spirv does not support SHADER_I8_DOT.\n");
+                    ret = VKD3D_ERROR_NOT_IMPLEMENTED;
+                    goto end;
+                }
+            }
+            else if (compiler_args->target_extensions[i] == VKD3D_SHADER_TARGET_EXTENSION_SCALAR_BLOCK_LAYOUT)
+            {
+                static const dxil_spv_option_scalar_block_layout helper =
+                        { { DXIL_SPV_OPTION_SCALAR_BLOCK_LAYOUT }, DXIL_SPV_TRUE };
+                if (dxil_spv_converter_add_option(converter, &helper.base) != DXIL_SPV_SUCCESS)
+                {
+                    ERR("dxil-spirv does not support SCALAR_BLOCK_LAYOUT.\n");
                     ret = VKD3D_ERROR_NOT_IMPLEMENTED;
                     goto end;
                 }
