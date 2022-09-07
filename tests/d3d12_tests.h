5711cee3991a6f065ac6a7b455930812bc7eb6f6
Hans-Kristian Arntzen <post@arntzen-software.no>
2022-09-07 12:47:03 +0200

    tests: Add test for in-group UAV coherence.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- tests/d3d12_tests.h
+++ tests/d3d12_tests.h
@@ -322,3 +322,5 @@ decl_test(test_fence_wait_robustness_shared);
 decl_test(test_root_signature_empty_blob);
 decl_test(test_sparse_buffer_memory_lifetime);
 decl_test(test_rendering_no_attachments_layers);
+decl_test(test_memory_model_uav_coherence_thread_group_dxbc);
+decl_test(test_memory_model_uav_coherence_thread_group_dxil);
