2de663d69449ddec67790a859c2067620d69b9bd
Hans-Kristian Arntzen <post@arntzen-software.no>
2023-01-30 13:31:41 +0100

    vkd3d: Actually query BOTTOM_LEVEL pointers properly.
    Just happened to work on NV for some reason.
    Fixes test failure on RADV.
    
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/acceleration_structure.c
+++ libs/vkd3d/acceleration_structure.c
@@ -361,7 +361,8 @@ static void vkd3d_acceleration_structure_write_postbuild_info(
             d3d12_command_list_reset_query(list, vk_query_pool, vk_query_index);
 
             VK_CALL(vkCmdWriteAccelerationStructuresPropertiesKHR(list->vk_command_buffer,
-                    1, &vk_acceleration_structure, vk_query_type, vk_query_pool, vk_query_index));
+                    1, &vk_acceleration_structure, VK_QUERY_TYPE_ACCELERATION_STRUCTURE_SERIALIZATION_BOTTOM_LEVEL_POINTERS_KHR,
+                    vk_query_pool, vk_query_index));
             VK_CALL(vkCmdCopyQueryPoolResults(list->vk_command_buffer,
                     vk_query_pool, vk_query_index, 1,
                     vk_buffer, offset + sizeof(uint64_t), stride,
