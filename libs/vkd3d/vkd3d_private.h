50997d006b51176eba7ed4099f70100dbc65296e
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2023-03-03 15:08:41 +0100

    vkd3d: Use core functionality for VK_KHR_format_feature_flags2.
    Core and required in 1.3.
    
    Signed-off-by: Philip Rebohle <philip.rebohle@tu-dortmund.de>

--- libs/vkd3d/vkd3d_private.h
+++ libs/vkd3d/vkd3d_private.h
@@ -117,7 +117,6 @@ struct vkd3d_vulkan_info
     bool KHR_ray_query;
     bool KHR_fragment_shading_rate;
     bool KHR_create_renderpass2;
-    bool KHR_format_feature_flags2;
     bool KHR_shader_atomic_int64;
     bool KHR_copy_commands2;
     bool KHR_dynamic_rendering;
