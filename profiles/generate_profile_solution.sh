2fbc52932ca6bc96cb2e6892f0fd59ad05a06fd8
Hans-Kristian Arntzen <post@arntzen-software.no>
2023-05-22 12:30:19 +0200

    meta: Add Vulkan profiles.

---
+++ profiles/generate_profile_solution.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p profiles/vulkan/debug
+
+if [ -z ${VULKAN_PROFILE_PREFIX} ]; then
+	VULKAN_PROFILE_PREFIX="$HOME/.local"
+	echo "Using VULKAN_PROFILE_PREFIX=$VULKAN_PROFILE_PREFIX."
+fi
+
+# Install Vulkan-Profiles to ~/.local prefix.
+
+python3 "${VULKAN_PROFILE_PREFIX}/share/vulkan/registry/gen_profiles_solution.py" \
+	--registry subprojects/Vulkan-Headers/registry/vk.xml \
+	--input . \
+	--output-library-inc profiles/vulkan \
+	--output-library-src profiles/vulkan \
+	--output-doc profiles/PROFILES.md \
+	--validate --debug
