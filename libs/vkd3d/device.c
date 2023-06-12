c58ffc8dcc090691977447ccca7d47dbbf9a13c7
Hans-Kristian Arntzen <post@arntzen-software.no>
2023-06-12 18:22:53 +0200

    vkd3d: Expose root signature v1.2.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/device.c
+++ libs/vkd3d/device.c
@@ -3740,14 +3740,14 @@ static HRESULT STDMETHODCALLTYPE d3d12_device_CheckFeatureSupport(d3d12_device_i
                 return E_INVALIDARG;
             }
 
-            if (!data->HighestVersion || data->HighestVersion > D3D_ROOT_SIGNATURE_VERSION_1_1)
+            if (!data->HighestVersion || data->HighestVersion > D3D_ROOT_SIGNATURE_VERSION_1_2)
             {
                 WARN("Unrecognized root signature version %#x.\n", data->HighestVersion);
                 return E_INVALIDARG;
             }
 
             TRACE("Root signature requested %#x.\n", data->HighestVersion);
-            data->HighestVersion = min(data->HighestVersion, D3D_ROOT_SIGNATURE_VERSION_1_1);
+            data->HighestVersion = min(data->HighestVersion, D3D_ROOT_SIGNATURE_VERSION_1_2);
 
             TRACE("Root signature version %#x.\n", data->HighestVersion);
             return S_OK;
