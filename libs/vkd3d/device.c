3648a4c31c8101d1ab94d75b00a3d9623dfc771a
Hans-Kristian Arntzen <post@arntzen-software.no>
2023-06-15 15:27:29 +0200

    vkd3d: Add config flag to enable experimental features.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/device.c
+++ libs/vkd3d/device.c
@@ -785,6 +785,7 @@ static const struct vkd3d_debug_option vkd3d_config_options[] =
     {"force_compute_root_parameters_push_ubo", VKD3D_CONFIG_FLAG_FORCE_COMPUTE_ROOT_PARAMETERS_PUSH_UBO},
     {"skip_driver_workarounds", VKD3D_CONFIG_FLAG_SKIP_DRIVER_WORKAROUNDS},
     {"curb_memory_pso_cache", VKD3D_CONFIG_FLAG_CURB_MEMORY_PSO_CACHE},
+    {"enable_experimental_features", VKD3D_CONFIG_FLAG_ENABLE_EXPERIMENTAL_FEATURES},
 };
 
 static void vkd3d_config_flags_init_once(void)
