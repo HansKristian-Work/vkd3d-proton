ca702c940e4ac3000eb0c3d143b8456c4a65ceb2
Hans-Kristian Arntzen <post@arntzen-software.no>
2023-09-07 21:58:54 +0200

    vkd3d: Split DGCC + async compute submissions in RADV.
    Works around a driver bug.
    
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/vkd3d_private.h
+++ libs/vkd3d/vkd3d_private.h
@@ -2673,6 +2673,7 @@ struct d3d12_command_list_sequence
      * where INDIRECT_ARGUMENT barriers appear in the stream. */
     struct d3d12_command_list_iteration iterations[VKD3D_MAX_COMMAND_LIST_SEQUENCES];
     unsigned int iteration_count;
+    bool uses_dgc_compute_in_async_compute;
 
     /* Emit normal commands here. */
     VkCommandBuffer vk_command_buffer;
@@ -2981,6 +2982,7 @@ struct d3d12_command_queue_submission_execute
 #endif
 
     bool debug_capture;
+    bool split_submission;
 };
 
 struct d3d12_command_queue_submission_bind_sparse
