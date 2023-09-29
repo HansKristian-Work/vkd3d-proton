4bf519dfd372fe0663230daf71cfd12a78660667
Hans-Kristian Arntzen <post@arntzen-software.no>
2023-09-29 17:51:12 +0200

    tests: Add MSAD DXIL test.
    Used by FSR3.
    
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- tests/d3d12_tests.h
+++ tests/d3d12_tests.h
@@ -367,3 +367,4 @@ decl_test(test_enhanced_barrier_split_barrier);
 decl_test(test_enhanced_barrier_discard_behavior);
 decl_test(test_enhanced_barrier_subresource);
 decl_test(test_enhanced_barrier_self_copy);
+decl_test(test_instruction_msad_dxil);
