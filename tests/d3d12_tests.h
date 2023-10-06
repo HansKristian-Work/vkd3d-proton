2ec2aab3357308c230451d356184a9d22e884c6e
Hans-Kristian Arntzen <post@arntzen-software.no>
2023-10-06 14:21:18 +0200

    tests: Add test_sampler_feedback_min_mip_level_array
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- tests/d3d12_tests.h
+++ tests/d3d12_tests.h
@@ -377,6 +377,7 @@ decl_test(test_dynamic_index_strip_cut);
 decl_test(test_depth_bias_behaviour);
 decl_test(test_sampler_feedback_resource_creation);
 decl_test(test_sampler_feedback_min_mip_level);
+decl_test(test_sampler_feedback_min_mip_level_array);
 decl_test(test_sampler_feedback_decode_encode_min_mip);
 decl_test(test_sampler_feedback_mip_used_region_level);
 decl_test(test_sampler_feedback_grad);
