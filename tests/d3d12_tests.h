2e99915ae6241e1d97d81c3eebbb9c0e0af14ce8
Hans-Kristian Arntzen <post@arntzen-software.no>
2023-10-19 13:09:39 +0200

    tests: Add test_sampler_feedback_resource_creation.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- tests/d3d12_tests.h
+++ tests/d3d12_tests.h
@@ -375,4 +375,4 @@ decl_test(test_sampler_non_normalized_coordinates);
 decl_test(test_dynamic_depth_bias);
 decl_test(test_dynamic_index_strip_cut);
 decl_test(test_depth_bias_behaviour);
-decl_test(test_line_rasterization);
+decl_test(test_sampler_feedback_resource_creation);
