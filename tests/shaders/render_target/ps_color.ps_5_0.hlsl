7f5ab292225d7e77f01c94fec4a7648fc3df3e84
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2024-02-12 17:41:31 +0100

    tests: Port render target test shaders to new system.
    Signed-off-by: Philip Rebohle <philip.rebohle@tu-dortmund.de>

---
+++ tests/shaders/render_target/ps_color.ps_5_0.hlsl
@@ -0,0 +1,6 @@
+float4 color;
+
+float4 main(float4 position : SV_POSITION) : SV_Target
+{
+    return color;
+}
---
+++ tests/shaders/render_target/ps_render_a8.ps_5_0.ps_6_0.hlsl
@@ -0,0 +1,4 @@
+void main(out float4 target : SV_Target)
+{
+    target = float4(0.0f, 0.25f, 0.5f, 1.0f);
+}
