4deb3150fd25c5035e0565cd46fe63159b9ffbd8
Hans-Kristian Arntzen <post@arntzen-software.no>
2024-02-16 08:50:18 +0100

    vkd3d: Add app_debug_marker_only config option.
    When capturing games that insert their own markers, adding our own
    markers on top gets very confusing.
    
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/device.c
+++ libs/vkd3d/device.c
@@ -911,6 +911,7 @@ static const struct vkd3d_debug_option vkd3d_config_options[] =
     {"disable_uav_compression", VKD3D_CONFIG_FLAG_DISABLE_UAV_COMPRESSION},
     {"disable_depth_compression", VKD3D_CONFIG_FLAG_DISABLE_DEPTH_COMPRESSION},
     {"disable_color_compression", VKD3D_CONFIG_FLAG_DISABLE_COLOR_COMPRESSION},
+    {"app_debug_marker_only", VKD3D_CONFIG_FLAG_APP_DEBUG_MARKER_ONLY},
 };
 
 static void vkd3d_config_flags_init_once(void)
