5daf2dfe160e1ae4bc9023716cf8a7186d895b43
Hans-Kristian Arntzen <post@arntzen-software.no>
2024-05-13 12:38:55 +0200

    vkd3d: Add more sophisticated barrier bisect support.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/vkd3d_private.h
+++ libs/vkd3d/vkd3d_private.h
@@ -3448,6 +3448,12 @@ struct vkd3d_breadcrumb_command_list_trace_context
     uint32_t next;
 };
 
+struct vkd3d_barrier_hash_range
+{
+    vkd3d_shader_hash_t lo;
+    vkd3d_shader_hash_t hi;
+};
+
 struct vkd3d_breadcrumb_tracer
 {
     /* There is room for N live command lists in this system.
@@ -3468,7 +3474,7 @@ struct vkd3d_breadcrumb_tracer
     pthread_mutex_t lock;
 
     pthread_mutex_t barrier_hash_lock;
-    vkd3d_shader_hash_t *barrier_hashes;
+    struct vkd3d_barrier_hash_range *barrier_hashes;
     size_t barrier_hashes_size;
     uint32_t barrier_hashes_count;
 
