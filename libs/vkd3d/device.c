2f520c7c923edf0e145ba08f8ff787cd605f37ca
Hans-Kristian Arntzen <post@arntzen-software.no>
2024-07-30 10:35:04 +0200

    vkd3d: Experimentally enable workgraphs behind config flag.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/device.c
+++ libs/vkd3d/device.c
@@ -8188,8 +8188,11 @@ static void d3d12_device_caps_init_feature_options21(struct d3d12_device *device
 {
     D3D12_FEATURE_DATA_D3D12_OPTIONS21 *options21 = &device->d3d12_caps.options21;
 
-    options21->WorkGraphsTier = D3D12_WORK_GRAPHS_TIER_NOT_SUPPORTED;
-    /* Tier 1_1 requires emulating DrawID */
+    options21->WorkGraphsTier = (vkd3d_config_flags & VKD3D_CONFIG_FLAG_ENABLE_EXPERIMENTAL_FEATURES) &&
+            device->device_info.shader_maximal_reconvergence_features.shaderMaximalReconvergence ?
+            D3D12_WORK_GRAPHS_TIER_1_0 : D3D12_WORK_GRAPHS_TIER_NOT_SUPPORTED;
+
+    /* Tier 1_1 requires EXT_dgc token. */
     options21->ExecuteIndirectTier = D3D12_EXECUTE_INDIRECT_TIER_1_0;
     options21->SampleCmpGradientAndBiasSupported = device->d3d12_caps.max_shader_model >= D3D_SHADER_MODEL_6_8 &&
             device->d3d12_caps.options14.AdvancedTextureOpsSupported;
