f0d08b2ca065f6a067fd4e079068b5d355b90935
Hans-Kristian Arntzen <post@arntzen-software.no>
2024-08-06 13:05:17 +0200

    Create some PSO variants.

--- include/vkd3d_shader.h
+++ include/vkd3d_shader.h
@@ -1004,6 +1004,7 @@ struct vkd3d_shader_node_input_data
     const char *node_share_input_id;
     uint32_t node_share_input_array_index;
     uint32_t local_root_arguments_table_index;
+    uint32_t is_indirect_bda_stride_program_entry_spec_id;
     bool dispatch_grid_is_upper_bound; /* [NodeMaxDispatchGrid] if true. */
     bool node_track_rw_input_sharing; /* Payload is tagged with [NodeTrackRWInputSharing]. */
     bool is_program_entry; /* [NodeIsProgramEntry] */
