0d7e8c394f56fed6d16027565a5811e9a243c2ad
Hans-Kristian Arntzen <post@arntzen-software.no>
2024-08-20 13:24:23 +0200

    vkd3d: Add config option to opt-in to ReBAR on 8 GB GPUs.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/device.c
+++ libs/vkd3d/device.c
@@ -933,6 +933,7 @@ static const struct vkd3d_debug_option vkd3d_config_options[] =
     {"disable_depth_compression", VKD3D_CONFIG_FLAG_DISABLE_DEPTH_COMPRESSION},
     {"disable_color_compression", VKD3D_CONFIG_FLAG_DISABLE_COLOR_COMPRESSION},
     {"app_debug_marker_only", VKD3D_CONFIG_FLAG_APP_DEBUG_MARKER_ONLY},
+    {"small_vram_rebar", VKD3D_CONFIG_FLAG_SMALL_VRAM_REBAR},
 };
 
 static void vkd3d_config_flags_init_once(void)
