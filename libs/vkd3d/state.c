5ade37e85d0c37adf8533a3a9471535c3913f7de
Hans-Kristian Arntzen <post@arntzen-software.no>
2024-09-03 13:39:55 +0200

    vkd3d: Add config flag for forced dynamic MSAA.
    Seems to match native driver behavior better, but this is outside D3D12
    spec, and we don't want to invalidate every single Fossilize archive
    ever.
    
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/state.c
+++ libs/vkd3d/state.c
@@ -3927,7 +3927,9 @@ void vkd3d_fragment_output_pipeline_free(struct hash_map_entry *entry, void *use
 static bool d3d12_graphics_pipeline_needs_dynamic_rasterization_samples(const struct d3d12_graphics_pipeline_state *graphics)
 {
     /* Ignore the case where the pipeline is compiled for a single sample since Vulkan drivers are robust against that. */
-    if (graphics->rs_desc.rasterizerDiscardEnable || graphics->ms_desc.rasterizationSamples == VK_SAMPLE_COUNT_1_BIT)
+    if (graphics->rs_desc.rasterizerDiscardEnable ||
+            (graphics->ms_desc.rasterizationSamples == VK_SAMPLE_COUNT_1_BIT &&
+            !(vkd3d_config_flags & VKD3D_CONFIG_FLAG_FORCE_DYNAMIC_MSAA)))
         return false;
 
     return graphics->rtv_active_mask || graphics->dsv_format ||
