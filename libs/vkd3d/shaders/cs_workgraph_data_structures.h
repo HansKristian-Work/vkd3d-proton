0099ce80bcb918cf2a892cc13ccccd00f1ab6b64
Hans-Kristian Arntzen <post@arntzen-software.no>
2024-10-11 15:16:54 +0200

    Implement a cursed dynamic amplifier.

--- libs/vkd3d/shaders/cs_workgraph_data_structures.h
+++ libs/vkd3d/shaders/cs_workgraph_data_structures.h
@@ -18,8 +18,8 @@ struct IndirectCommands
 	uint end_elements; // Read by node as input metadata in coalesce / thread mode.
 	uint linear_offset_atomic; // Used by expander to write unrolled data.
 	uint total_fused_elements;
-	uint padding0;
-	uint padding1;
+	uint expander_workgroup_counter; // Used by payload exander.
+	uint expander_total_groups; // Used by payload expander.
 };
 
 #endif
