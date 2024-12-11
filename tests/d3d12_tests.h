7263f4b69512dfd5a9d4a99a20ff768d9292fdee
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2024-12-11 15:49:37 +0100

    vkd3d: Add test for copying between compressed and non-compressed images.
    Signed-off-by: Philip Rebohle <philip.rebohle@tu-dortmund.de>

--- tests/d3d12_tests.h
+++ tests/d3d12_tests.h
@@ -160,6 +160,7 @@ decl_test(test_vertex_id_dxil);
 decl_test(test_copy_texture);
 decl_test(test_copy_texture_ds_edge_cases);
 decl_test(test_copy_texture_buffer);
+decl_test(test_copy_texture_bc_rgba);
 decl_test(test_copy_buffer_texture);
 decl_test(test_copy_block_compressed_texture);
 decl_test(test_copy_buffer_overlap);
