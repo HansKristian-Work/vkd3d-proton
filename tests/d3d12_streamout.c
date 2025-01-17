0321739a97cc612f9c82ea5337c4da2e256c1338
Mike Blumenkrantz <michael.blumenkrantz@gmail.com>
2025-01-17 13:10:32 -0500

    tests: bug_if on adreno drivers for fails in test_primitive_restart_list_topology_stream_output
    this hardware exhibits nonconformant behavior
    
    Signed-off-by: Mike Blumenkrantz <michael.blumenkrantz@gmail.com>

--- tests/d3d12_streamout.c
+++ tests/d3d12_streamout.c
@@ -155,8 +155,10 @@ void test_primitive_restart_list_topology_stream_output(void)
     get_buffer_readback_with_command_list(counter_buffer, DXGI_FORMAT_R32_UINT, &rb, queue, command_list);
     counter = get_readback_uint(&rb, 0, 0, 0);
 
-    /* RDNA2 hardware bug from what we understand. */
-    bug_if(is_radv_device(context.device))
+    /* RDNA2 hardware bug from what we understand.
+     * adreno hardware omits degenerate triangles
+     */
+    bug_if(is_radv_device(context.device) || is_adreno_device(context.device))
     ok(counter == sizeof(expected_output), "Got unexpected counter %u, expected %u.\n",
             counter, (unsigned int)sizeof(expected_output));
 
@@ -168,8 +170,10 @@ void test_primitive_restart_list_topology_stream_output(void)
         const struct vec4 *expected = &expected_output[i];
         data = get_readback_vec4(&rb, i, 0);
 
-        /* RDNA2 hardware bug from what we understand. */
-        bug_if(is_radv_device(context.device))
+        /* RDNA2 hardware bug from what we understand.
+        * adreno hardware omits degenerate triangles
+        */
+        bug_if(is_radv_device(context.device) || is_adreno_device(context.device))
         ok(compare_vec4(data, expected, 1),
                 "Got {%.8e, %.8e, %.8e, %.8e}, expected {%.8e, %.8e, %.8e, %.8e}.\n",
                 data->x, data->y, data->z, data->w, expected->x, expected->y, expected->z, expected->w);
@@ -452,4 +456,3 @@ void test_vertex_shader_stream_output_dxil(void)
 {
     test_vertex_shader_stream_output(true);
 }
-
