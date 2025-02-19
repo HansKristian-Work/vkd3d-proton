c8f176169599c3c6ca6cd4c813ddad2713ab4129
Hans-Kristian Arntzen <post@arntzen-software.no>
2025-02-19 15:09:46 +0100

    tests: Add test for missing nonuniform on PS varying input.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- tests/d3d12_tests.h
+++ tests/d3d12_tests.h
@@ -436,3 +436,4 @@ decl_test(test_destruction_notifier_interfaces);
 decl_test(test_sparse_default_mapping);
 decl_test(test_varying_nointerpolation_mixed_type_dxbc);
 decl_test(test_varying_nointerpolation_mixed_type_dxil);
+decl_test(test_divergent_buffer_index_varying);
