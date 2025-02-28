78fdaa1d4120f9a86678f48d971e63f793cb6219
Hans-Kristian Arntzen <post@arntzen-software.no>
2025-02-28 17:46:52 +0100

    tests: Add test for OOB alloca.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- tests/d3d12_tests.h
+++ tests/d3d12_tests.h
@@ -442,3 +442,5 @@ decl_test(test_gs_topology_mismatch_dxil);
 decl_test(test_sm67_helper_lane_only_wave_ops);
 decl_test(test_constant_lut_out_of_bounds_dxbc);
 decl_test(test_constant_lut_out_of_bounds_dxil);
+decl_test(test_alloca_out_of_bounds_dxbc);
+decl_test(test_alloca_out_of_bounds_dxil);
