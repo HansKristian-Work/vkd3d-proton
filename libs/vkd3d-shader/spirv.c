6d30e905bb3a19c9e6fdb4c79f1e483fff9ecc44
Hans-Kristian Arntzen <post@arntzen-software.no>
2025-04-02 12:18:18 +0200

    frog: Hook up VK_KHR_cooperative_matrix.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d-shader/spirv.c
+++ libs/vkd3d-shader/spirv.c
@@ -12184,6 +12184,10 @@ void vkd3d_shader_extract_feature_meta(struct vkd3d_shader_code *code)
                     meta |= VKD3D_SHADER_META_FLAG_USES_SUBGROUP_OPERATIONS;
                     break;
 
+                case SpvCapabilityCooperativeMatrixKHR:
+                    meta |= VKD3D_SHADER_META_FLAG_USES_COOPERATIVE_MATRIX;
+                    break;
+
                 default:
                     break;
             }
