fb866d84fa912d4304d3f567ceb028b00d27bc4c
Hans-Kristian Arntzen <post@arntzen-software.no>
2025-10-03 12:42:26 +0200

    tests: Add test for batched resolves.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- tests/d3d12_tests.h
+++ tests/d3d12_tests.h
@@ -495,3 +495,4 @@ decl_test(test_root_signature_embedded);
 decl_test(test_tight_resource_alignment);
 decl_test(test_raytracing_acceleration_structure_validation);
 decl_test(test_copy_batch);
+decl_test(test_resolve_batch);
