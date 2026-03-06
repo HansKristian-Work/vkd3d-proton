9ce577a17e4fad00c57e7e25f96f62f2ac1b35b5
Hans-Kristian Arntzen <post@arntzen-software.no>
2026-03-06 17:39:44 +0100

    tests: Add MSAA path to COPY queue depth test.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- tests/d3d12_tests.h
+++ tests/d3d12_tests.h
@@ -550,3 +550,4 @@ decl_test(test_copy_queue_buffer_image);
 decl_test(test_copy_queue_render_target);
 decl_test(test_copy_queue_depth_stencil);
 decl_test(test_copy_queue_render_target_msaa);
+decl_test(test_copy_queue_depth_stencil_msaa);
