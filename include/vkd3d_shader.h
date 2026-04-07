38ad703b625b6c4c328f05cd5b9b02be83c31e3e
Hans-Kristian Arntzen <post@arntzen-software.no>
2026-04-07 13:12:34 +0200

    vkd3d-shader: Add shader quirk for full UAV coherency.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- include/vkd3d_shader.h
+++ include/vkd3d_shader.h
@@ -547,6 +547,8 @@ enum vkd3d_shader_target_extension
 /* Use precise fma instead of the spec correct mad. To workaround issues with invariance. */
 #define VKD3D_SHADER_QUIRK_PRECISE_FMA (1ull << 33)
 
+#define VKD3D_SHADER_QUIRK_FULL_UAV_COHERENCY (1ull << 34)
+
 typedef uint64_t vkd3d_shader_quirks_t;
 
 struct vkd3d_shader_quirk_hash
