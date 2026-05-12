2b487ac36b3a97ede0124154ed8bd26a281c8c75
Hans-Kristian Arntzen <post@arntzen-software.no>
2026-05-12 11:36:19 +0200

    vkd3d: Add shader quirk workaround for PRAGMATA.
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- libs/vkd3d/device.c
+++ libs/vkd3d/device.c
@@ -1017,6 +1017,16 @@ static const struct vkd3d_shader_quirk_info spiderman2_quirks = {
     spiderman2_hashes, ARRAY_SIZE(spiderman2_hashes), 0,
 };
 
+/* These shaders clamp the wave size to 32, but misses this in a few places of course ... */
+static const struct vkd3d_shader_quirk_hash pragmata_hashes[] = {
+    { "PersistentClusterCulling", 0, VKD3D_SHADER_QUIRK_CLAMP_WAVE_SIZE_TO_THREAD_GROUP32 },
+    { "PersistentShadowClusterCulling", 0, VKD3D_SHADER_QUIRK_CLAMP_WAVE_SIZE_TO_THREAD_GROUP32 },
+};
+
+static const struct vkd3d_shader_quirk_info pragmata_quirks = {
+    pragmata_hashes, ARRAY_SIZE(pragmata_hashes), 0,
+};
+
 static const struct vkd3d_shader_quirk_meta application_shader_quirks[] = {
     /* F1 2020 (1080110) */
     { VKD3D_STRING_COMPARE_EXACT, "F1_2020_dx12.exe", &f1_2019_2020_quirks },
@@ -1097,6 +1107,8 @@ static const struct vkd3d_shader_quirk_meta application_shader_quirks[] = {
     { VKD3D_STRING_COMPARE_ENDS_WITH, "-Shipping.exe", &ue4_quirks },
 	/* Spider Man 2 (2651280) */
     { VKD3D_STRING_COMPARE_EXACT, "Spider-Man2.exe", &spiderman2_quirks },
+    /* PRAGMATA (3357650) */
+    { VKD3D_STRING_COMPARE_STARTS_WITH, "PRAGMATA", &pragmata_quirks },
     /* MSVC fails to compile empty array. */
     { VKD3D_STRING_COMPARE_NEVER, NULL, NULL },
 };
