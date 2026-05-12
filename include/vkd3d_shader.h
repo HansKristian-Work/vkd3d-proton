56166c3c70c43cf6e5cc7435871fef91493c957d
Hans-Kristian Arntzen <post@arntzen-software.no>
2026-05-12 11:35:17 +0200

    vkd3d-shader: Add shader quirk to clamp reported wave size.
    For thread groups of 32 threads, some shaders just assume that WaveSize
    will be maximum 32.
    
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- include/vkd3d_shader.h
+++ include/vkd3d_shader.h
@@ -551,6 +551,10 @@ enum vkd3d_shader_target_extension
 /* Use precise fma instead of the spec correct mad. To workaround issues with invariance. */
 #define VKD3D_SHADER_QUIRK_PRECISE_FMA (1ull << 33)
 
+/* If the threadgroup size is 32, lower reported WaveSize to shader accordingly.
+ * Some games assume they can get away with only <= wave32. */
+#define VKD3D_SHADER_QUIRK_CLAMP_WAVE_SIZE_TO_THREAD_GROUP32 (1ull << 34)
+
 typedef uint64_t vkd3d_shader_quirks_t;
 
 struct vkd3d_shader_quirk_hash
