7ad4937e28ea1a0e78e3d5345a5c420b6f1ba549
Hans-Kristian Arntzen <post@arntzen-software.no>
2026-08-11 16:10:23 +0200

    tests: Add test for UB r16 texel buffer atomics
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- tests/d3d12_tests.h
+++ tests/d3d12_tests.h
@@ -582,3 +582,4 @@ decl_test(test_query_heap_cpu_resolve_occlusion);
 decl_test(test_buffer_descriptor_byte_offset);
 decl_test(test_raytracing_collection_handle_invariance);
 decl_test(test_copy_block_spam);
+decl_test(test_r16_texel_buffer_atomic);
