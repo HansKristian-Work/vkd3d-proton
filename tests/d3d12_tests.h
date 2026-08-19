655d6a2a6f8fd6da81d297b970329a43c26a90a7
Hans-Kristian Arntzen <post@arntzen-software.no>
2026-08-19 11:59:04 +0200

    tests: Add opacity micromap test with ray query in SM 6.9
    Signed-off-by: Hans-Kristian Arntzen <post@arntzen-software.no>

--- tests/d3d12_tests.h
+++ tests/d3d12_tests.h
@@ -389,6 +389,7 @@ decl_test(test_raytracing_deferred_compilation);
 decl_test(test_raytracing_mismatch_global_rs_link);
 decl_test(test_raytracing_null_rtas);
 decl_test(test_raytracing_opacity_micro_map);
+decl_test(test_raytracing_opacity_micro_map_ray_query);
 decl_test(test_raytracing_huge_dispatch);
 decl_test(test_fence_wait_robustness);
 decl_test(test_fence_wait_robustness_shared);
